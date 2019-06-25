d2b153d16d8e745b8e88502cb14891dd22882059
haluskua <haluskua.one@gmail.com>
2019-06-25 22:37:23 +1200

    added port 443

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./server')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 443
 
 app.listen(port, function () {
   // eslint-disable-next-line no-console
